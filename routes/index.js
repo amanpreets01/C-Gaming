7da189912c99e622813483119683ec00b5e8afc7
root <amanpreets879@gmail.com>
2019-07-08 22:42:27 +0530

    Little frontend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.hbs', {title: 'Dashboard'});
+  res.render('index.hbs', {title: 'C-Gaming'});
 });
 
 module.exports = router;
