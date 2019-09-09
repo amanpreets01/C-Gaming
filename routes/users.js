9446f40b76aa92f5d50afb7979d1b900dd336a1e
Amanpreet Singh <amanpreets879@gmail.com>
2019-09-09 16:51:32 +0530

    'Small_changes'

--- routes/users.js
+++ routes/users.js
@@ -62,30 +62,42 @@ router.post('/signup' , (req , res) => {
 	});
 
 });
+router.get('/games/:user_id' , (req , res , next) => {
+	
+		MongoClient.connect(mongo_url , (err, client) => {
+		console.log(req.session.email);
+		client.db('games').collection('all_games').find({}).toArray(function(err, doc) {
+			console.log(doc);
 
-router.get('/games/:user_id' , (req , res) => {
-	console.log(session.user_id);
-	if(session.user_id == ''){
-		return res.redirect('/');
-	}
-	else{
-		user_id = req.params.user_id;
-		MongoClient.connect(mongo_url ,(err , client) => {
-			client.db('games').collection('user_games').findOne({user_id})
-			.then((doc) => {
-				if(doc == null){
-					res.render('games' , {'self_games' : 'Empty . Purchase some to play some'})
-				}
-				else{
-					console.log(doc);
-					res.render('games' , {'self_games' : doc.games});
-				}
-			})
-			.catch(err => console.log(err));
+			for(var i = 0 ; i < doc.length	 ; i++){
+				
+				all_games.push(doc[i].name);
+			}
+			});
+		client.db('games').collection('user_games').find({'email' : req.session.email}).toArray(function(err, doc) {
+			console.log(doc);
 
+			for(var i = 0 ; i < doc.length	 ; i++){
+				purchased.push(doc[i].game);
+			}
+			console.log(purchased)
+			for(var i = 0 ; i < purchased.length ; i++){
+				for ( var  j = 0 ; j < all_games.length ; j++){
+					if (purchased[i].localeCompare(all_games[j]) == 0){
+
+						var index = all_games.indexOf(all_games[j]);
+						if (index > -1) {
+						  all_games.splice(index, 1);
+						}
+					}
+				}
+			}
+
+			console.log(all_games);
+
+			res.render('games' , {all_games , purchased});
+			});
 		});
-	}
-
 });
 
 module.exports = router;
